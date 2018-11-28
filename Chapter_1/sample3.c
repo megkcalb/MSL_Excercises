2d8b202da718c17bca4da894e063cd1e46c5ce9f
Meganathan M <megkcalb@gmail.com>
2018-11-28 13:09:40 +0530

    char and line count

--- Chapter_1/sample3.c
+++ Chapter_1/sample3.c
@@ -3,8 +3,12 @@
 int main()
 {
     double cc;
+    int lc=0;
     for(cc=0;getchar()!=EOF;cc++)
+        if(cc == '\n')
+            lc++;
     ;
-    printf("Character count : %f",cc);
+    printf("Character count : %f\n Line count : %d",cc,lc+1);
     return 0;
+    
 }
