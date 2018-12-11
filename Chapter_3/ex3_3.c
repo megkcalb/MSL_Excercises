0ddcd6d57e0e108f12fa3158c021775fbe158399
Meganathan M <megkcalb@gmail.com>
2018-12-11 15:29:40 +0530

    expanding a-1 and 0-9

--- Chapter_3/ex3_3.c
+++ Chapter_3/ex3_3.c
@@ -6,36 +6,30 @@ void expand(char s1[], char s2[]) {
     char c, d, e;
     int i, j;
     i = j = 0;
-
-    while ('\0' != (c = s1[i++])) {
-        if (' ' != c && '-' == s1[i] && '\0' != s1[i + 1]) {
+    while((c=s1[i++])!='\0')
+    {
+        if(s1[i]=='-' && s1[i+1] >= c){
             i++;
-            d = s1[i];
-            if (d < c) {
-                while (c > d) {
-                    s2[j++] = c--;
-                }
-            }
-            else {
-                while (c < d) {
-                    s2[j++] = c++;
-                }
+            while(c<s1[i]){
+                s2[j++] = c++;
             }
         }
-        else {
+        else
+        {
             s2[j++] = c;
         }
+        s2[j] = '\0';
     }
-    s2[j] = '\0';
+   
 }
 
+int main() {
 
-main() {
 
-
-    char s1[512] = "-a-z 0-9 a-d-f -0-2 some text 1-1 WITH CAPITALS! 0-0 5-3 -";
+    char s1[512] = "a-z 0-9";
     char s2[512];
     expand(s1, s2);
     printf("%s\n", s2);
 
+    return 0;
 }
