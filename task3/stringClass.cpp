393e44ffdb2ce77af5fe0674c1848db4914ad2cb
Meganathan M <megkcalb@gmail.com>
2019-01-28 00:10:33 +0530

    Update stringClass.cpp

--- task3/stringClass.cpp
+++ task3/stringClass.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-
+#include<stdlib.h>
 using namespace std;
 
 //char *strapp(char*,char*);
@@ -8,27 +8,27 @@ using namespace std;
 //char *substr(char*, int , int);
 class stringop
 {
-     public : 
+     public :
           char *strapp(char*,char*);
           int indexof(char*, char);
-          int len(char*);
-          char *substr(char*, int , int);
+         int len(char*);
+         char *substr1(char*, int , int);
 };
 int main(){
-      
+
       stringop obj;
-      char str1[] = "meg" ;
-      char str2[] = "getup" ;
-      
+      char str1[100] = "meg" ;
+      char str2[100] = "getup" ;
+
       cout << " appended string : " << obj.strapp(str1,str2) << endl;
       cout << " index : " <<  obj.indexof(str2,'i') << endl;
-      cout << " length of the string is : " << obj.len(str2) << endl;
-      //cout << str2[-1]<< endl;
-      cout << " sub string : " << obj.substr(str2,1,4) << endl;
+     cout << " length of the string is : " << obj.len(str2) << endl;
+      cout << str2[-1]<< endl;
+     cout << " sub string : " << obj.substr1(str2,1,4) << endl;
       return 0;
 }
 
-char stringop::*strapp(char *str1, char *str2)
+char* stringop::strapp(char *str1, char *str2)
 {
     char *op= str1;
 
@@ -52,15 +52,16 @@ char stringop::*strapp(char *str1, char *str2)
 int stringop::indexof(char *str, char ch)
 {
       for(int i = -1; str[i] != '\0'; i++){
-   		   
+
 	   if( str[i] == ch )
 		   return i+1 ;
           }
       return -1;
 }
 
+
 int stringop::len(char *str){
-         
+
 	int l = 1;
 
 	while( *str != '\0'){
@@ -71,16 +72,16 @@ int stringop::len(char *str){
 	return l;
 }
 
-char stringop::*substr(char *str,int start, int end ){
+char* stringop::substr1(char *str,int start, int end ){
 
-        char* op1 = (char*) malloc( (end - start +1)*sizeof(char)) ;
+        char* op1 = (char*) malloc( (end - start +2)*sizeof(char)) ;
 //	char* op1 = (char*) malloc(end) ;
         int i , j ;
 	for( i = start ,  j = 0; i <= end ; i++,j++){
-                  
+
 	          op1[j] = str[i];
          }
 	op1[j] = '\0';
-	      
-        return op1;    
+
+        return op1;
 }
