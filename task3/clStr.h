ea4c6954bc3caf492f08cd14d35ff7bcd69815a1
Meganathan M <megkcalb@gmail.com>
2019-01-28 18:16:51 +0530

    header file consisting the string operations

---
+++ task3/clStr.h
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+class stringop
+{
+     public :
+ 
+         char *strapp(char*,char*);
+         int indexof(char*, char);
+         int len(char*);
+         char *substr1(char*, int , int);
+};
+
