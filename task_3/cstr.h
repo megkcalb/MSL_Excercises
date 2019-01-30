0b13b0025a88423e2df6577582c76f863dc2c0a2
Meganathan M <megkcalb@gmail.com>
2019-01-30 17:42:23 +0530

    Update cstr.h

--- task_3/cstr.h
+++ task_3/cstr.h
@@ -8,14 +8,16 @@ class cstr{
 public:
     cstr();
     cstr(const char * source);
-    ~cstr();
     int len();
     int index_of( char c);
     char* substr ( int start, int end ) ;
     friend cstr operator+(cstr &str1, cstr &str2);
+    friend std::ostream& operator<< (std::ostream&, const cstr &string);
+    ~cstr();
 private:
     int copy(const char * source);
     void massive_copy(const char * source, char * destination, int init_point, int string_length);
     int strlen(const char * source);
 
 };
+
